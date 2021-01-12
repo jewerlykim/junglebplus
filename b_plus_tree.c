59934efb5c716751868d17c89da9b774cf0d5978
2014100890 <31299657+2014100890@users.noreply.github.com>
2021-01-13 02:11:30 +0900

    finish

--- b_plus_tree.c
+++ b_plus_tree.c
@@ -18,26 +18,209 @@ void insert_node(NODE *curr, int curr_key, NODE **root, int degree);
 void connect_leaf(NODE *left_sibling, NODE *right_sibling);
 NODE *leaf_split(NODE *curr, int degree);
 NODE *parent_split(NODE *curr, int degree); // child가 있을 때
-void delete_node(NODE *curr, int key, NODE **root);
+void delete_node(NODE *curr, int key, NODE **root, int degree);
 void search_node(NODE **address, NODE *curr, int key);
+void recursive_delete(NODE *curr, NODE **root, int degree);
+int search_rightsmall(NODE *curr);
 
-void delete_node(NODE *curr, int key, NODE **root)
+int search_rightsmall(NODE *curr)
 {
-    NODE **address;
-    address = (NODE **)malloc(sizeof(NODE *)*2);
-    memset(address, 0, sizeof(NODE *)*2); 
-    // delete->search address in nothing return null 
-    search_node(address, curr, key); // Delete하기 위해서  
-    if (address[0] == NULL){
-        printf("없다"); 
-        return; 
+    if (curr->child[0] == NULL)
+    {
+        return curr->key[0];
     }
-
-
+    else
+    {
+        return search_rightsmall(curr->child[0]);
+    }
 }
 
-void search_node(NODE **address, NODE *curr, int key) // 받기전에 주소 받을 것 malloc 해라 
-{   
+void recursive_delete(NODE *curr, NODE **root, int degree)
+{
+    NODE *tmp;
+
+    if ((curr->key_count) >= (degree - degree / 2 - 1)) // 지웠을때에도 최소길이를 만족해요^~^
+    {
+        return;
+    }
+    else
+    {                             // 지웠을때 최소길이 만족이 안됨;;;;;;;;;;;;;;;;;;;;;고난 시작
+        if (curr->parent == NULL) // 단군할아버지다
+        {                         // 부모 없음
+            return;               //사실 난 자식도없었고 부모도 없었다 ㅎㅎ;;
+        }
+        else // 부모가 있엉!
+        {
+            int child_idx = 0;
+            for (int i = 0; i <= curr->parent->key_count; i++) // 나는 몇번째 자식인가
+            {
+                if (curr->parent->child[i] == curr) // 너는 내 자식이 아니란다.
+                {
+                    child_idx = i;
+                }
+            }
+
+            if (child_idx == curr->parent->key_count) // 내가 막내라니 (젤 오른쪽)
+            {
+                NODE *left_sibling = curr->parent->child[child_idx - 1];                       // 내 왼쪽 형제를 찾는다.
+                left_sibling->key[left_sibling->key_count] = curr->parent->key[child_idx - 1]; // 부모를 내려준다
+                (curr->parent->key_count)--;                                                   // 나중에 부모 다 없어졌는지 확인해야대;; 언제 누가하는데? 바로 나
+                (left_sibling->key_count)++;
+                connect_leaf(left_sibling, curr->next);
+
+                for (int i = 1; i < curr->key_count; i++) // 부모랑 key 0 같으니 0번 친구 건들면 안됨
+                {
+                    left_sibling->key[left_sibling->key_count] = curr->key[i];
+                    left_sibling->child[left_sibling->key_count] = curr->child[i];
+
+                    if (curr->child[i] != NULL)
+                    {
+                        curr->child[i]->parent = left_sibling;
+                    }
+
+                    (left_sibling->key_count)++;
+                }
+
+                left_sibling->child[left_sibling->key_count] = curr->child[curr->key_count];
+                // check
+                if (curr->child[curr->key_count] != NULL)
+                {
+                    curr->child[curr->key_count]->parent = left_sibling;
+                }
+                // free(curr);
+                tmp = left_sibling;
+
+                if ((left_sibling->key_count) >= (degree)) // left sibling이 다 찼는지 확인해서 다 찼으면 up을 해줘
+                {
+                    up(left_sibling->key[degree / 2], left_sibling, root, degree);
+                }
+            }
+            else // 젤 오른쪽 아닐 때
+            {
+                NODE *right_sibling = curr->parent->child[child_idx + 1];
+                curr->key[curr->key_count] = curr->parent->key[child_idx];
+                (curr->key_count)++;
+                (curr->parent->key_count)--;
+                connect_leaf(curr, right_sibling->next);
+                for (int i = 1; i < right_sibling->key_count; i++)
+                {
+                    curr->key[curr->key_count] = right_sibling->key[i];
+                    curr->child[curr->key_count] = right_sibling->child[i];
+                    if (right_sibling->child[i] != NULL)
+                    {
+                        right_sibling->child[i]->parent = curr;
+                    }
+                    (curr->key_count)++;
+                }
+                curr->child[curr->key_count] = right_sibling->child[right_sibling->key_count];
+                if (right_sibling->child[right_sibling->key_count] != NULL)
+                {
+                    right_sibling->child[right_sibling->key_count]->parent = curr;
+                }
+                // free(right_sibling);
+                for (int i = child_idx; i < curr->parent->key_count; i++)
+                {
+                    curr->parent->key[i] = curr->parent->key[i + 1];         // 부모 왼쪽으로 1보 이동!
+                    curr->parent->child[i + 1] = curr->parent->child[i + 2]; // 자식들도 이동!
+                }
+                tmp = curr;
+
+                if ((curr->key_count) >= (degree))
+                { //내가 꽉찼어!!!~!~!~!~!~
+                    up(curr->key[degree / 2], curr, root, degree);
+                }
+            }
+        }
+
+        if ((curr->parent->key_count) < (degree - degree / 2 - 1))
+        { // 부모 가 최소개수가 안돼...
+            if (curr->parent->parent == NULL)
+            { // 내 부모가 단군이라니!!!!!!! 이제 곧 내가 단군이오 ㅎㅎ ;;
+                if (curr->parent->key_count == 0)
+                {
+                    *root = tmp;
+                    tmp->parent = NULL;
+                }
+                return;
+            }
+            recursive_delete(curr->parent, root, degree);
+        }
+    }
+}
+
+void delete_node(NODE *curr, int key, NODE **root, int degree)
+{
+    NODE **address;
+    address = (NODE **)malloc(sizeof(NODE *) * 2);
+    memset(address, 0, sizeof(NODE *) * 2);
+    search_node(address, curr, key); // Delete하기 위해서
+    if (address[0] == NULL)
+    {
+        printf("없다\n");
+        return;
+    }
+    else
+    {                 // 리프에만 있을 때
+        int idx = -1; // 내 위치 찾기
+        for (int i = 0; i < address[0]->key_count; i++)
+        {
+            if (address[0]->key[i] == key)
+            {
+                idx = i;
+                break;
+            }
+        }
+
+        if (address[0]->child[0] == NULL)
+        { // 자식이 없네???? 리프네??!!
+            for (int j = idx; j < (address[0]->key_count); j++)
+            { // 땡겨주기 baby~~
+                address[0]->key[j] = address[0]->key[j + 1];
+            }
+
+            (address[0]->key_count)--; // 키 개수 하나 줄여줌 이게 끝일까? 과연???
+            recursive_delete(address[0], root, degree);
+        }
+
+        if (address[1] != NULL)
+        {
+            memset(address, 0, sizeof(NODE *) * 2);
+            search_node(address, *root, key);
+            if (address[1] != NULL)
+            {
+                for (int i = 0; i < address[1]->key_count; i++)
+                {
+                    if (address[1]->key[i] == key)
+                    {
+                        idx = i;
+                        break;
+                    }
+                }
+                address[1]->key[idx] = search_rightsmall(address[1]->child[idx + 1]);
+            }
+            if (address[0] != NULL)
+            {
+                for (int i = 0; i < address[0]->key_count; i++)
+                {
+                    if (address[0]->key[i] == key)
+                    {
+                        idx = i;
+                        break;
+                    }
+                }
+                for (int j = idx; j < (address[0]->key_count); j++)
+                { // 땡겨주기 baby~~
+                    address[0]->key[j] = address[0]->key[j + 1];
+                }
+                (address[0]->key_count)--; // 키 개수 하나 줄여줌 이게 끝일까? 과연???
+                recursive_delete(address[0], root, degree);
+            }
+        }
+    }
+}
+
+void search_node(NODE **address, NODE *curr, int key) // 받기전에 주소 받을 것 malloc 해라
+{
     if (curr->key_count == 0) // 만약 tree를 만들지 않고 search 할 경우
     {
         printf("먼저 키를 삽입해주세요.");
@@ -46,14 +229,16 @@ void search_node(NODE **address, NODE *curr, int key) // 받기전에 주소 받
     int i;
     for (i = 0; i < curr->key_count; i++)
     {
-        if (curr->key[i] == key) // 찾았을 때 
+        if (curr->key[i] == key) // 찾았을 때
         {
-            if (curr->child[0] == NULL){ // 리프인지 아닌지 확인 
+            if (curr->child[0] == NULL)
+            { // 리프인지 아닌지 확인
                 address[0] = curr;
                 return;
             }
-            else{
-                address[1] = curr; 
+            else
+            {
+                address[1] = curr;
             }
         }
         if (curr->key[i] > key)
@@ -68,7 +253,7 @@ void search_node(NODE **address, NODE *curr, int key) // 받기전에 주소 받
     }
     if (i == curr->key_count)
     {
-        if (curr->child[0] == 0) // 없어요 
+        if (curr->child[0] == 0) // 없어요
         {
             return;
         }
@@ -268,11 +453,11 @@ int main()
     // scanf("%d", &degree);
     NODE *root = create_node(degree); // 창조신 만듬
     int insert_nbr_flag[100000] = {0};
-    
+
     NODE **address;
-    address = (NODE **)malloc(sizeof(NODE *)*2);
-    memset(address, 0, sizeof(NODE *)*2); 
-    
+    address = (NODE **)malloc(sizeof(NODE *) * 2);
+    memset(address, 0, sizeof(NODE *) * 2);
+
     int key1 = 30;
     int key2 = 40;
     int key3 = 50;
@@ -310,8 +495,10 @@ int main()
     insert_node(root, key10, &root, degree);
 
     search_node(address, root, 40);
-    printf("%d\n", address[0]->next->key[0]);
-    delete_node(root, 70, &root); 
+    // printf("%d\n", address[0]->next->key[0]);
+    delete_node(root, 50, &root, degree);
+    delete_node(root, 60, &root, degree);
+    delete_node(root, 30, &root, degree);
 
     // insert_node(root, key11, &root, degree);
     // insert_node(root, key12, &root, degree);
@@ -328,11 +515,11 @@ int main()
     // insert_node(root, key23, &root, degree);
     // insert_node(root, key24, &root, degree);
 
-    printf("%d\n", root->child[0]->child[0]->key_count);
-    printf("%d\n", root->child[0]->child[0]->next->key_count);
-    printf("%d\n", root->child[0]->child[0]->next->next->key_count);
-    printf("%d\n", root->child[0]->child[0]->next->next->next->key_count);
-    printf("%d\n", root->child[0]->child[0]->next->next->next->next->key_count);
+    printf("%d\n", root->key[0]);
+    printf("%d\n", root->child[0]->next->key[0]);
+    printf("%d\n", root->child[0]->next->next->key[0]);
+    printf("%d\n", root->child[0]->next->next->next->key[0]);
+    // printf("%d\n", root->child[0]->next->next->next->next->key[0]);
 
     // while (1)
     // {
